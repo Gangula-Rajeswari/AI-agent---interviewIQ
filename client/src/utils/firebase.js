ae8a010390d4d943450db0fb3299c874d8dea6ef
Gangula-Rajeswari <143239685+Gangula-Rajeswari@users.noreply.github.com>
2026-04-18 15:27:39 +0530

    Update firebase.js

--- client/src/utils/firebase.js
+++ client/src/utils/firebase.js
@@ -1,21 +1,4 @@
-/*
-import { initializeApp } from "firebase/app";
-import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
-const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_APIKEY,
-  authDomain: "interviewiq-35815.firebaseapp.com",
-  projectId: "interviewiq-35815",
-  storageBucket: "interviewiq-35815.firebasestorage.app",
-  messagingSenderId: "351998106503",
-  appId: "1:351998106503:web:11618b67605d48365b9122",
-  measurementId: "G-RX51F9RV8F"
-};
-
-const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
-const provider = new GoogleAuthProvider();
-export {auth , provider}*/
 
 import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
