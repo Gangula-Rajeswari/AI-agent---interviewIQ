f77864ab07c3a4a94083497900fbbcfb7fd589a4
Gangula-Rajeswari <143239685+Gangula-Rajeswari@users.noreply.github.com>
2026-04-18 14:15:33 +0530

    Update auth.controller.js

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -32,7 +32,7 @@ export const googleAuth = async (req, res) => {
         res.cookie("token", token, {
             httpOnly: true,
             secure: true, 
-            sameSite: "lax",
+            sameSite: "none",
             path: "/",
             maxAge: 7 * 24 * 60 * 60 * 1000
         });
@@ -59,7 +59,7 @@ export const logOut = async (req, res) => {
     try {
         res.clearCookie("token", {
             httpOnly: true,
-            sameSite: "lax",
+            sameSite: "none",
             secure: true,
             path: "/"
         });
